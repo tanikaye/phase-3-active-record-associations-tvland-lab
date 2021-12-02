da69c57bf5e1016cc1e7b04bdccffecabebc9442
Tani Kaye <1tanikaye@gmail.com>
2021-12-02 09:29:55 -0500

    commit tree 2e60611fd3e0ffbf8ad92534477a8e9c89714a87

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :Characters
+
+  def characters
+    self.characters
+  end
+
+  def shows
+    self.shows
+  end
+
+  def full_name
+    "#{self.first_name} + #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.map do |t|
+      "#{t.name} + #{t.show.name}"
+    end
+  end
+
 end
